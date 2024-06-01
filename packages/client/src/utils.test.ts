dec9d0a3ae9691a7558a04fc44986c91edb7da3c
徐青山 <1370572852@qq.com>
2024-06-01 23:03:21 +0800

    11

--- packages/client/src/utils.test.ts
+++ packages/client/src/utils.test.ts
@@ -1,30 +1,30 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it } from "vitest"
 
-import { nullthrows } from "./utils";
+import { nullthrows } from "./utils"
 
 describe("nullthrows function", () => {
   it("should return the value when it is not null or undefined", () => {
-    const value = "test";
-    const result = nullthrows(value);
-    expect(result).toBe(value);
-  });
+    const value = "test"
+    const result = nullthrows(value)
+    expect(result).toBe(value)
+  })
 
   it("should throw an error when the value is null", () => {
-    const value = null;
-    const action = () => nullthrows(value);
-    expect(action).toThrow("[sandpack-client]: Value is nullish");
-  });
+    const value = null
+    const action = () => nullthrows(value)
+    expect(action).toThrow("[sandpack-client]: Value is nullish")
+  })
 
   it("should throw an error when the value is undefined", () => {
-    const value = undefined;
-    const action = () => nullthrows(value);
-    expect(action).toThrow("[sandpack-client]: Value is nullish");
-  });
+    const value = undefined
+    const action = () => nullthrows(value)
+    expect(action).toThrow("[sandpack-client]: Value is nullish")
+  })
 
   it("should throw an error with a custom message when the value is null or undefined", () => {
-    const value = null;
-    const customErrorMessage = "Custom error message";
-    const action = () => nullthrows(value, customErrorMessage);
-    expect(action).toThrow(`[sandpack-client]: ${customErrorMessage}`);
-  });
-});
+    const value = null
+    const customErrorMessage = "Custom error message"
+    const action = () => nullthrows(value, customErrorMessage)
+    expect(action).toThrow(`[sandpack-client]: ${customErrorMessage}`)
+  })
+})
